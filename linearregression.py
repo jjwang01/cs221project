be111d90be8c473925526766e77505229818e474
Justin Wang <jjwang01@stanford.edu>
2019-05-23 17:17:19 -0700

    Got rid of LabelEncoder warning. More succinct code.

--- linearregression.py
+++ linearregression.py
@@ -22,11 +22,15 @@ result = pd.merge(diagnoses_icd, admissions[['SUBJECT_ID', 'HOSPITAL_EXPIRE_FLAG
 
 # use linear regression on numpy arrays to try to predict the expire flag
 # one-hot encode ICD9_CODE b/c it's a categorical variable, avoid misrepresenting data
+onehot_encoder = preprocessing.OneHotEncoder(sparse=True)
+onehot_encoded = onehot_encoder.fit_transform(result['ICD9_CODE'].astype(str).to_numpy().reshape(-1,1))
+"""
 label_encoder = preprocessing.LabelEncoder()
 integer_encoded = label_encoder.fit_transform(result['ICD9_CODE'].astype(str).to_numpy())
 onehot_encoder = preprocessing.OneHotEncoder(sparse=True)
 integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
 onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+"""
 y = result['HOSPITAL_EXPIRE_FLAG'].to_numpy()
 X_train, X_test, y_train, y_test = train_test_split(onehot_encoded, y, test_size=0.2, random_state=0)
 
